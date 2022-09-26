13aa88cd40b567d0ab5e9c9056437736c230c603
PiotrKapusta135 <piotrkap97@gmail.com>
2022-09-26 22:02:48 +0200

    Project init

--- env.py
+++ env.py
@@ -4,5 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 class Environment:
-    def __init__(self):
+
+    def __init__(self, df, window, features, min_performance, start=0, end=None, mu=None, std=None):
+        self.df = df
+        self.window = window
+        self. features = features
+        self.min_performance = min_performance
+        self.start = start
+        self.end = end
+        self.mu = mu
+        self.std = std
