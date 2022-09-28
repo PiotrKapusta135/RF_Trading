7ce298d6a7ce9e17e5885954900f523604cd47f5
PiotrKapusta135 <piotrkap97@gmail.com>
2022-09-28 23:29:13 +0200

    env creation 1

--- env.py
+++ env.py
@@ -7,12 +7,24 @@ import seaborn as sns
 
 class Environment:
 
-    def __init__(self, df, window, features, min_performance, start=0, end=None, mu=None, std=None):
+    def __init__(self, df, window, min_performance=0.65, start=0, end=None, initial_balance=1000):
         self.df = df
         self.window = window
-        self. features = features
         self.min_performance = min_performance
         self.start = start
         self.end = end
-        self.mu = mu
-        self.std = std
+        self.total_steps = len(self.df)
+        self.initial_balance = initial_balance
+        self.min_account_balance = 0
+        self.min_transaction_value = 10
+        self.total_fee = 0
+
+        self.action_space = np.arange(0, 1, 0.01)
+        self.observation_space = np.arange(0, self.window)
+
+    def reset(self):
+        self.balance = initial_balance
+        self.performance = performance
+
+
+
